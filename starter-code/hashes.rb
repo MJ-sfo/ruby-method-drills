985c44616670f070a3a360f80f667e46b11a53de
MJ-sfo <michaeljlaird@yahoo.com>
2017-05-17 16:33:32 -0700

    i sent earlier commits to wrong depository

--- starter-code/hashes.rb
+++ starter-code/hashes.rb
@@ -6,6 +6,22 @@
   # counts how many times each character appears in a string
   # ignores case
   # returns the hash
+def character_count text
+  # split into array
+  # itterate over array - if not in hash, add with value =1
+  # else add one to value
+  new_hash = {}
+  arr = text.downcase.split("")
+  arr.each do |index|
+    if new_hash.key?(index)
+      new_hash[index] += 1
+    else
+      new_hash[index]=1
+    end  #  if
+  end  #  arr.each
+  new_hash
+end
+p character_count("Daaave")
 
 ## STRETCH ##
 #word_count
--- starter-code/iterators.rb
+++ starter-code/iterators.rb
@@ -5,26 +5,70 @@
   # takes in a string
   # counts the spaces in a string
   # returns number of spaces
+def count_spaces text
+  arr = text.split("")
+  count = 0
+  arr.each do |i|
+    if (i == " ")
+      count +=1
+    end
+  end
+  count
+end
+# p count_spaces("dave loves sue ")
 
 #string_lengths
   # takes in an array of strings
   # returns an array containing the lengths of the strings
+def string_lengths arr
+  len = []
+  arr.each {|i| len.push(i.length) }
+  len
+end
+# p string_lengths(["dave", 'solutions of test', 'wombat'])
 
 #remove_falsy_values
   # takes in a list
   # filters out falsy values from a list
   # returns the updated list
+def remove_falsy_values arr
+  truisms = []
+  arr.each do |i|
+    if !!i
+       truisms.push(i)
+    end
+  end
+  truisms
+end
+list = [3, false, "String", nil, "20-31", true, "", "Other String"]
+# p remove_falsy_values(list)
 
 #exclude_last
   # takes in an array or string
   # removes the last item from the array or string
   # returns it
+def exclude_last obj
+  if obj.class == Array
+    obj = obj.reverse.drop(1).reverse
+  else
+    obj = obj[0...-1]
+  end
+  obj
+end
+# p exclude_last([0,1,2])
+# p exclude_last("Sussan")
+
 
 #exclude_first
   # takes in an array or string
   # removes the first item from an array
   # removes the first character from a string
   # returns a new string - does not alter the original input (non-destructive)
+def exclude_first obj
+
+end
+
+
 
 #exclude_ends
   # takes in an array or string
